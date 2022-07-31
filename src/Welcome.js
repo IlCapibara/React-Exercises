4d60ab17be316244a826ae8d4914a4b7e247a6c4
Gianluigi <gianluigicolameo99@gmail.com>
2022-07-31 12:14:22 +0200

    Commit esercizio conditional rendering 3

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,7 @@ export class Welcome extends React.Component{
         return (
             <>
                 <p>Welcome, {this.props.name ?? "Gian"}</p>
-                {<Age age={(this.props.age !== undefined) && 30}/>}
+                {<Age age={(18 > this.props.age > 65) && 30}/>}
             </>
         )
     }
